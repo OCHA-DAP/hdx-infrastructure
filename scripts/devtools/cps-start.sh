94ea85a7d79b0f1658df0cb99c4ff90447d745d2
Serban Teodorescu <teodorescu.serban@gmail.com>
2014-04-09 00:49:29 +0300

    initial add cps scripts

---
+++ scripts/devtools/cps-start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+	echo "You need to be root to run me.";
+	exit 1;
+fi
+
+cps_location="/opt/www/tomcat";
+
+sudo -u tomcat $cps_location/tomcat/control.sh start > /dev/null
+
+if [ $? -ne 0 ]; then
+	echo "Command failed.";
+	exit 2;
+fi
+
+echo "Success!"
