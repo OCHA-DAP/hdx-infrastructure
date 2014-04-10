737a515f2d9b8424c85de92571ffde415f373537
Serban Teodorescu <teodorescu.serban@gmail.com>
2014-04-10 13:46:43 +0300

    some ckan scripts added

---
+++ scripts/devtools/ckan-start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $(id -u) -ne 0 ]; then
+	echo "You need to be root to run me.";
+	exit 1;
+fi
+
+invoke-rc.d supervisor start > /dev/null
+
+if [ $? -ne 0 ]; then
+	echo "Command failed.";
+	exit 2;
+fi
+
+echo "Success!"
