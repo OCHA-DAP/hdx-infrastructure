0ed70b22198345f8e2900becf157c4b9064da791
Serban Teodorescu <teodorescu.serban@gmail.com>
2014-05-29 17:12:30 +0300

    first draft

--- scripts/devtools/ckan-sysadmin-list.sh
+++ scripts/devtools/ckan-sysadmin-list.sh
@@ -13,8 +13,27 @@ function list_sysadmins {
 	paster sysadmin list -c $ckan_ini_file
 }
 
+function pretty_list_sysadmins {
+	user_list=$(paster sysadmin list -c $ckan_ini_file | grep -E "^name=");
+	for user in $(echo $user_list | sed -e 's/^User name=//; s/ display=.*//'); do
+		pretty_list_user_details $user;
+	done
+}
+
+function pretty_list_user_details {
+	user_string=$(paster user $1 -c $ckan_ini_file | grep -E "^<User ")
+	user_username=$(echo $user_string | sed -e 's/.* name=//; s/ openid=.*//');
+	user_fullname=$(echo $user_string | sed -e 's/.* fullname=//; s/ email=.*//');
+	user_email=$(echo $user_string | sed -e 's/.* email=//; s/ apikey=.*//');
+	echo "$user_username,user_email,user_fullname"
+}
+
 activate;
-list_sysadmins;
+if [ $1 -eq "-p" ]; then
+	pretty_list_sysadmins;
+else
+	list_sysadmins;
+fi
 deactivate;
 cd $curr_dir;
 
--- scripts/devtools/ckan-user-list.sh
+++ scripts/devtools/ckan-user-list.sh
@@ -9,16 +9,6 @@ fi
 # (ckan and cps location, branches etc)
 . $(which devtoolconfig.sh)
 
-username=""
-password=""
-email=""
-
-function activate {
-	cd $ckan_base_dir
-	. bin/activate
-	cd src/ckan
-}
-
 function list_users {
 	paster user list -c $ckan_ini_file 2> /dev/null
 	if [ $? -ne 0 ]; then
@@ -27,8 +17,27 @@ function list_users {
 	fi
 }
 
+function pretty_list_users {
+	user_list=$(paster user -c $ckan_ini_file | grep -E "^name=");
+	for user in $(echo $user_list | sed -e 's/^User name=//; s/ display=.*//'); do
+		pretty_list_user_details $user;
+	done
+}
+
+function pretty_list_user_details {
+	user_string=$(paster user $1 -c $ckan_ini_file | grep -E "^<User ")
+	user_username=$(echo $user_string | sed -e 's/.* name=//; s/ openid=.*//');
+	user_fullname=$(echo $user_string | sed -e 's/.* fullname=//; s/ email=.*//');
+	user_email=$(echo $user_string | sed -e 's/.* email=//; s/ apikey=.*//');
+	echo "$user_username,user_email,user_fullname"
+}
+
 activate;
-list_users;
+if [ $1 -eq "-p" ]; then
+	pretty_list_users;
+else
+	list_users;
+fi
 deactivate;
 cd $curr_dir;
 
