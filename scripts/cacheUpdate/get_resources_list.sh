8dd672f692ca43e4f72a6b01efd3f0aa19c3dc7c
Serban Teodorescu <teodorescu.serban@gmail.com>
2014-10-01 17:32:22 +0300

    changed get reources list script on cacheUpdate

--- scripts/cacheUpdate/get_resources_list.sh
+++ scripts/cacheUpdate/get_resources_list.sh
@@ -1,7 +1,4 @@
 # get all cps resources published in ckan
-psql -U ckanuser ckandb -tc "select url from resource where url like '%http://manage.hdx.rwlabs.org/hdx/api/exporter/%' and state = 'active';" > cps_resources
-
-# not required anymore, wfp is on prod now :)
-# mangle wfp location.
-#cat cps_resources | grep -v \/wfp\/ > cps_resources_and_wfp
-#cat cps_resources | grep \/wfp\/ | sed -e 's/\/manage\./\/test-manage\./' >> cps_resources_and_wfp
+psql -U ckanuser ckandb -tc \
+    "select url from resource where url like '%http://manage.hdx.rwlabs.org/hdx/api/exporter/%' and state = 'active';" | \
+    grep -vE "^$" | sed -e 's/^ //' > cps_resources
