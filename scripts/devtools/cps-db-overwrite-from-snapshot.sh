1c20707fa1d0dddc91a46fdbac8a9b31bd7dc4bb
Serban Teodorescu <teodorescu.serban@gmail.com>
2014-10-01 16:50:46 +0300

    checked and tested

--- scripts/devtools/cps-db-overwrite-from-snapshot.sh
+++ scripts/devtools/cps-db-overwrite-from-snapshot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ $(id -u) -ne 0 ]; then
-	echo "You need to be root to run me.";
-	exit 1;
+        echo "You need to be root to run me.";
+        exit 1;
 fi
 
 # includes the config file to define YOUR specific parameters
@@ -12,23 +12,28 @@ fi
 . $(which cps-db-get-snapshot.sh)
 
 function drop_db {
-	echo -en "Droping all tables from $cps_sql_db... "
-	# drop db
-	paster db clean -c $cps_ini_file > /dev/null
-	if [ $? -ne 0 ]; then
-		echo -en "failed.\n";
-		exit 3;
-	fi
+        echo -en "Droping db $cps_sql_db... "
+        # drop db
+        sudo -u postgres dropdb $cps_sql_db > /dev/null
+        # create an empty one
+        sudo -u postgres createdb -O $cps_sql_user $cps_sql_db -E utf-8 > /dev/null
+        if [ $? -ne 0 ]; then
+                echo -en "failed.\n";
+                exit 3;
+        fi
 }
 
 function recreate_db_from_backup {
-	# recreate the empty db
-	echo "Restoring db from the prod backup. Only errors will be shown."
-	psql -U $cps_sql_user $cps_sql_db -f $cps_tmp_dir/$cps_db_backup > /dev/null
-	if [ $? -ne 0 ]; then
-		echo "create database failed.";
-		exit 4;
-	fi
+        # recreate the empty db
+        echo "Decompressing archive file..."
+        gunzip -f $cps_tmp_dir/$cps_db_backup > /dev/null
+        echo "Restoring db from the prod backup. Only errors will be shown."
+        uncompressed_file=$(echo $cps_db_backup | sed -e 's/.gz$//')
+        psql -U $cps_sql_user $cps_sql_db -f $cps_tmp_dir/$uncompressed_file > /dev/null
+        if [ $? -ne 0 ]; then
+                echo "create database failed.";
+                exit 4;
+        fi
 }
 
 # main
@@ -40,7 +45,6 @@ drop_db;
 recreate_db_from_backup;
 deactivate;
 cps-start.sh;
-cps-reindex.sh;
 rm -rf $cps_tmp_dir;
 echo "Script completed."
 cd $curr_dir;
